79465fb6f94e82b73da6b2fdc4a7f2febe200aa9
TigranPatatanyan <patatanyan017@gmail.com>
2020-07-21 00:05:34 +0400

    changes

--- app/src/main/java/com/tigran/databindingtask/UserViewModel.kt
+++ app/src/main/java/com/tigran/databindingtask/UserViewModel.kt
@@ -1,5 +1,6 @@
 package com.tigran.databindingtask
 
+import android.graphics.Color.*
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -15,7 +16,19 @@ class UserViewModel : ViewModel() {
     private val _likes = MutableLiveData(0)
     var likes: LiveData<Int> = _likes
 
+    private var _color: MutableLiveData<Int> = MutableLiveData(BLUE)
+    var color: LiveData<Int> = _color
+
     fun onLike() {
         _likes.value = (_likes.value ?: 0) + 1
+        setColor(_likes.value!!)
+    }
+
+    fun setColor(like: Int) {
+        when {
+            like > 10 -> _color.value = RED
+            like > 5 -> _color.value = YELLOW
+            else -> _color.value = BLUE
+        }
     }
 }
